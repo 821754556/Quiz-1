9f450d76110c84a30c141e8027b1fd0b4581cee9
EdwardXie <EdwardXie@10.180.69.237>
2016-09-14 20:18:40 -0400

    second commit

--- src/quiz1Q1/Main.java
+++ src/quiz1Q1/Main.java
@@ -17,9 +17,25 @@ public class Main {
 		double INT = input.nextDouble();
 		
 		double a = (COMP / ATT - 0.3) * 5;
+		if (a > 2.375)
+			a = 2.375;
+		else if (a < 0)
+			a = 0;
 		double b = (YDS / ATT - 3) * 0.25;
+		if (b > 2.375)
+			b = 2.375;
+		else if (b < 0)
+			b = 0;
 		double c = (TD / ATT) * 20;
+		if (c > 2.375)
+			c = 2.375;
+		else if (c < 0)
+			c = 0;
 		double d = 2.375 - (INT / ATT * 25);
+		if (d > 2.375)
+			d = 2.375;
+		else if (d < 0)
+			d = 0;
 		
 		double QBrating = (a + b + c + d) / 6 * 100;
 		
