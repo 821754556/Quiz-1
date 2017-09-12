a190e7fabd174fa9980bac0915e2491e635dcdd0
mac <mac@10.0.0.106>
2017-09-13 02:58:08 +0800

    lol

--- src/quiz1Q1/Main.java
+++ src/quiz1Q1/Main.java
@@ -2,7 +2,6 @@ package quiz1Q1;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Number of passing attempts: ");
@@ -16,30 +15,32 @@ public class Main {
 		System.out.print("Interceptions: ");
 		double INT = input.nextDouble();
 		
-		double a = (COMP / ATT - 0.3) * 5;
-		if (a > 2.375)
-			a = 2.375;
-		else if (a < 0)
-			a = 0;
-		double b = (YDS / ATT - 3) * 0.25;
-		if (b > 2.375)
-			b = 2.375;
-		else if (b < 0)
-			b = 0;
-		double c = (TD / ATT) * 20;
-		if (c > 2.375)
-			c = 2.375;
-		else if (c < 0)
-			c = 0;
-		double d = 2.375 - (INT / ATT * 25);
+		double d = (COMP / ATT - 0.3) * 5;
 		if (d > 2.375)
 			d = 2.375;
 		else if (d < 0)
 			d = 0;
+		double c = (YDS / ATT - 3) * 0.25;
+		if (c > 2.375)
+			c = 2.375;
+		else if (c < 0)
+			c = 0;
+		double b = (TD / ATT) * 20;
+		if (b > 2.375)
+			b = 2.375;
+		else if (b < 0)
+			b = 0;
+		double a = 2.375 - (INT / ATT * 25);
+		if (a > 2.375)
+			a = 2.375;
+		else if (a < 0)
+			a = 0;
 		
 		double QBrating = (a + b + c + d) / 6 * 100;
 		
-		System.out.print("The QB rating for the 5 data you enter is: " + QBrating);
+		System.out.print("The QB rating is: " + QBrating);
 	}
-
 }
+	
+	
+
